83c4847777d6c900ef4a237659616d298b8dc4c0
Beto Sosa <betososah@gmail.com>
2018-06-20 23:09:42 -0500

    hola

--- mix.exs
+++ mix.exs
@@ -2,12 +2,15 @@ defmodule Testing.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :testing,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :testing,
+       version: "0.1.0",
+       elixir: "~> 1.4",
+       build_embedded: Mix.env == :prod,
+       start_permanent: Mix.env == :prod,
+       deps: deps(),
+       test_coverage: [tool: ExCoveralls],
+    ]
   end
 
   # Configuration for the OTP application
@@ -28,6 +31,8 @@ defmodule Testing.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:excoveralls, "~> 0.8", only: :test}
+    ]
   end
 end
